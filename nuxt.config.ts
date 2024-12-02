bdddbdbe8798f63cf03ce945c4c3678ac4bd3888
Salvador Cáceres <salvadorcaceresc@gmail.com>
2024-12-01 21:47:29 -0400

    fix: install tailwind by nuxt module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,10 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
+
   devServer: {
     port: 5173
-  }
+  },
+
+  modules: ['@nuxtjs/tailwindcss']
 })
